03abff77515ac2d9f05d55dac709b45612d07fcf
LJ <ljenloe@live.unc.edu>
2019-12-09 22:53:39 -0500

    commit

---
+++ calendar.js
@@ -0,0 +1,7 @@
+//here we need to have I think just one method that they can interact with?
+
+//this function renders the calendar. Probably doesn't need to be async but 
+//just wanted to put it there
+async function renderCalendar() {
+
+}
---
+++ connect.js
@@ -0,0 +1,24 @@
+//This will have the stuff that is.. the app. This will be the most complicated and we're probably
+//gonna have to reference the render file for a09 or something
+
+//this is to render the whole page. It will call the list render in it, which will call the 
+//membercard render, etc etc
+function renderPage() {
+
+}
+
+//render search bar ??
+
+function renderSearchBar() {
+
+}
+
+//this will render the list of profiles/people in the theatre community
+function renderList() {
+
+}
+
+//represents the section that holds each member's primary skills/experience/interests
+function renderMemberCard() {
+
+}
