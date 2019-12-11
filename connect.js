16088ce6c79c503a680e45df05d0afe0c5601289
LJ <ljenloe@live.unc.edu>
2019-12-11 15:20:31 -0500

    added a tab

--- connect.js
+++ connect.js
@@ -25,6 +25,20 @@ export async function renderPage() {
 
 export async function handleWelcomeButtonPress(event) {
 
+    /*
+    const result = await axios({
+        method: 'get',
+        url: 'http://localhost:3000/account/status',
+        withCredentials: true,
+    });
+
+    console.log(result);
+
+    //alert(result[2].fname);
+    */
+
+    
+
     let r = `
     <div class="container is-connect-box2" id="appBox">
 
@@ -52,13 +66,22 @@ export async function handleWelcomeButtonPress(event) {
             </select>
             <div class="field">
                             <div class="control" style="padding: 20px">
-                                    <input class="input is-rounded" type="text" placeholder="Search Companies">
+                                    <input class="input is-rounded" type="text" placeholder="Search">
                             </div>
                 </div>
-            <div style="padding-bottom: 10px">
+            <div style="padding-bottom: 30px">
                 <button type="submit" class="button is-white is-centered" id="submit">Search</button>
-            </div
+            </div>
+            <div class="columns">
+            <div class="column is-selected-left">
+                Users
+            </div>
+            <div class="column" style="color: #003365" id="posts">
+                Posts
+            </div>
+            </div>
             </form>
+            
         </div>
         </div>
         
@@ -133,8 +156,216 @@ export async function handleWelcomeButtonPress(event) {
     r+=`</div>`
 
     $root.on('click', "#request", handleRequestButtonPress);
+    $root.on('click', "#posts", handlePostsTabClick);
 
     $('#appBox').replaceWith(r);
+}
+
+export async function handlePostsTabClick(event) {
+    event.preventDefault();
+
+    let content = event.target.getAttribute("id");
+
+    let r;
+
+    if(content=="posts") {
+    r = `
+    <div class="container is-connect-box2" id="appBox">
+
+        <div>
+
+        <div class="is-top-bar">
+
+            <form>
+            <select>
+                <option value="any">Any</option>
+                <option value="actors">Acting</option>
+                <option value="directors">Directing</option>
+                <option value="producers">Producing</option>
+                <option value="costumes">Costuming</option>
+                <option value="props">Props</option>
+                <option value="set">Set</option>
+                <option value="makeup">Make-Up</option>
+                <option value="lighting">Lighting</option>
+                <option value="sound">Sound</option>
+                <option value="dramaturg">Dramaturg</option>
+                <option value="writing">Writing</option>
+                <option value="executive">Executive</option>
+                <option value="stagehand">Stagehand</option>
+                <option value="general">General</option>
+            </select>
+            <div class="field">
+                            <div class="control" style="padding: 20px">
+                                    <input class="input is-rounded" type="text" placeholder="Search">
+                            </div>
+                </div>
+            <div style="padding-bottom: 30px">
+                <button type="submit" class="button is-white is-centered" id="submit">Search</button>
+            </div>
+            <div class="columns">
+            <div class="column" style="color: #003365" id="users">
+                Users
+            </div>
+            <div class="column is-selected-right">
+                Posts
+            </div>
+            </div>
+            </form>
+            
+        </div>
+        </div>
+        
+    `;
+
+    for(let i=0; i<50; i++){
+        //alert("good lord");
+        r+=`
+            <div class="is-profile-card">
+            <div class="columns">
+                <div class="column">
+                        <img class="is-profile-pic" width="50" height="50" src="OldWell.png">
+                </div>
+                <div class="column">
+                    <b>Twig Man</b></br>
+                    <span style:"color: #F8DA17">Senior</span>
+                </div>
+                <div class="column">
+                    Lopper Dopper!
+                </div>
+                <div class="column">
+                    <button type="button" class="button is-warning" id="request">Request</button>
+                </div>
+            </div>
+            </div>
+            `;
+    }
+
+}
+else {
+
+    r = `
+    <div class="container is-connect-box2" id="appBox">
+
+        <div>
+
+        <div class="is-top-bar">
+
+            <form>
+            <select>
+                <option value="any">Any</option>
+                <option value="actors">Acting</option>
+                <option value="directors">Directing</option>
+                <option value="producers">Producing</option>
+                <option value="costumes">Costuming</option>
+                <option value="props">Props</option>
+                <option value="set">Set</option>
+                <option value="makeup">Make-Up</option>
+                <option value="lighting">Lighting</option>
+                <option value="sound">Sound</option>
+                <option value="dramaturg">Dramaturg</option>
+                <option value="writing">Writing</option>
+                <option value="executive">Executive</option>
+                <option value="stagehand">Stagehand</option>
+                <option value="general">General</option>
+            </select>
+            <div class="field">
+                            <div class="control" style="padding: 20px">
+                                    <input class="input is-rounded" type="text" placeholder="Search">
+                            </div>
+                </div>
+            <div style="padding-bottom: 30px">
+                <button type="submit" class="button is-white is-centered" id="submit">Search</button>
+            </div>
+            <div class="columns">
+            <div class="column is-selected-left">
+                Users
+            </div>
+            <div class="column" style="color: #003365" id="posts">
+                Posts
+            </div>
+            </div>
+            </form>
+            
+        </div>
+        </div>
+        
+    `;
+
+    for(let i=0; i<50; i++){
+        //alert("good lord");
+
+        if(i%2==0){
+        r+=`
+            <div class="is-profile-card">
+            <div class="columns">
+                <div class="column">
+                        <img class="is-profile-pic" width="50" height="50" src="OldWell.png">
+                </div>
+                <div class="column">
+                    <b>Twig Man</b></br>
+                    <span style:"color: #F8DA17">Senior</span>
+                </div>
+                <div class="column">
+                    <b>Director</b></br>
+                    Experienced
+                </div>
+                <div class="column">
+                    <b>Actor</b></br>
+                    Interested
+                </div>
+                <div class="column">
+                    <b>Producer</b></br>
+                    Competent
+                </div>
+                <div class="column">
+                    <button type="button" class="button is-warning" id="request">Request</button>
+                </div>
+            </div>
+            </div>
+            `;
+        }
+        else {
+
+            r+=`
+            <div class="is-profile-card">
+            <div class="columns">
+                <div class="column">
+                        <img class="is-profile-pic" width="50" height="50" src="OldWell.png">
+                </div>
+                <div class="column">
+                    Twig Man</br>
+                    <span style:"color: #F8DA17">Senior</span>
+                </div>
+                <div class="column">
+                    Director</br>
+                    Experienced
+                </div>
+                <div class="column">
+                    Actor</br>
+                    Interested
+                </div>
+                <div class="column">
+                    Producer</br>
+                    Competent
+                </div>
+                <div class="column">
+                    <button type="button" class="button is-danger" id="unavailable">Unavailable</button>
+                </div>
+            </div>
+            </div>
+            `;  
+
+        }
+    }
+    
+}
+
+    r+=`</div>`;
+
+    $('#appBox').replaceWith(r);
+    $root.on('click', "#users", handlePostsTabClick);
+
+
 }
 
 export async function handleRequestButtonPress(event) {
