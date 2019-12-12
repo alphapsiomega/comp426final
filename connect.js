21be888ba4c3732cdc13c01e6443562990cdc11a
LJ <ljenloe@live.unc.edu>
2019-12-11 23:48:06 -0500

    v

--- connect.js
+++ connect.js
@@ -5,10 +5,11 @@
 
 const $root = $('#root');
 
+/*
 let jwt = localStorage.getItem('jwt');
 console.log(jwt);
 let decode = parseJwt(jwt);
-
+*/
 
 //let jwt = localStorage.getItem('jwt')
 //console.log("jwt is " + jwt);
@@ -17,6 +18,7 @@ let decode = parseJwt(jwt);
 //membercard render, etc etc
 export async function renderPage() {
 
+    /*
     let appBox = `
         <div class="container has-text-centered is-connect-box" id="appBox">
             <div id="replace">
@@ -25,9 +27,139 @@ export async function renderPage() {
         </div>
     
     `;
+    */
 
-    $root.on('click', "#open", handleWelcomeButtonPress);
-    $root.append(appBox);
+    //$root.on('click', "#open", handleWelcomeButtonPress);
+    
+
+    let r = `
+    <div class="container is-connect-box2" id="appBox">
+
+        <div>
+
+        <div class="is-top-bar">
+
+            <div class="is-post-button" id="postBox">
+                Click here to post!
+            </div>
+
+            <form>
+            
+            <div class="field">
+                            <div class="control" style="padding-top: 20px; padding-left: 20px; padding-right: 20px;">
+                                    <input class="input is-rounded" type="text" placeholder="Search">
+                            </div>
+                </div>
+                <div style="padding-bottom: 20px">
+                <select>
+                <option value="any">Anything</option>
+                <option value="actors">Acting</option>
+                <option value="directors">Directing</option>
+                <option value="producers">Producing</option>
+                <option value="costumes">Costuming</option>
+                <option value="props">Props</option>
+                <option value="set">Set</option>
+                <option value="makeup">Make-Up</option>
+                <option value="lighting">Lighting</option>
+                <option value="sound">Sound</option>
+                <option value="dramaturg">Dramaturg</option>
+                <option value="writing">Writing</option>
+                <option value="executive">Executive</option>
+                <option value="stagehand">Stagehand</option>
+                <option value="general">General</option>
+            </select>
+            </div>
+            <div style="padding-bottom: 30px">
+                <button type="submit" class="button is-white is-centered" id="submit">Search</button>
+            </div>
+            <div class="columns">
+            <div class="column is-selected-left">
+                Users
+            </div>
+            <div class="column" style="color: #003365" id="posts">
+                Posts
+            </div>
+            </div>
+            </form>
+            
+        </div>
+        </div>
+        
+    `;
+
+    for(let i=0; i<50; i++){
+        //alert("good lord");
+
+        if(i%2==0){
+        r+=`
+            <div class="is-profile-card">
+            <div class="columns">
+                <div class="column">
+                        <img class="is-profile-pic" width="50" height="50" src="OldWell.png">
+                </div>
+                <div class="column">
+                    <b>Twig Man</b></br>
+                    <span style:"color: #F8DA17">Senior</span>
+                </div>
+                <div class="column">
+                    <b>Director</b></br>
+                    Experienced
+                </div>
+                <div class="column">
+                    <b>Actor</b></br>
+                    Interested
+                </div>
+                <div class="column">
+                    <b>Producer</b></br>
+                    Competent
+                </div>
+                <div class="column">
+                    <button type="button" class="button is-warning" id="request">Request</button>
+                </div>
+            </div>
+            </div>
+            `;
+        }
+        else {
+
+            r+=`
+            <div class="is-profile-card">
+            <div class="columns">
+                <div class="column">
+                        <img class="is-profile-pic" width="50" height="50" src="OldWell.png">
+                </div>
+                <div class="column">
+                    Twig Man</br>
+                    <span style:"color: #F8DA17">Senior</span>
+                </div>
+                <div class="column">
+                    Director</br>
+                    Experienced
+                </div>
+                <div class="column">
+                    Actor</br>
+                    Interested
+                </div>
+                <div class="column">
+                    Producer</br>
+                    Competent
+                </div>
+                <div class="column">
+                    <button type="button" class="button is-danger" id="unavailable">Unavailable</button>
+                </div>
+            </div>
+            </div>
+            `;  
+
+        }
+    }
+    r+=`</div>`
+
+    $root.on('click', "#postBox", handlePostBoxClick);
+    $root.on('click', "#request", handleRequestButtonPress);
+    $root.on('click', "#posts", handlePostsTabClick);
+
+    $root.append(r);
 
 }
 
@@ -35,18 +167,66 @@ export async function handlePostBoxClick(event) {
 
     event.preventDefault;
     let r = `<div id="textype">
-    <textarea class="postText"></textarea>
+    <textarea class="postText" id="textToPost"></textarea>
     </div>
-    <button type="submit" class="button is-black">Post</button>`
+    <button type="submit" class="button is-black" id="postbutton">Post</button>`
     
 
     $('#postBox').replaceWith(r);
+    $root.on('click', "#postbutton", handlePostButtonPress)
+
+}
+
+export async function handlePostButtonPress(event) {
+
+    event.preventDefault();
+
+    let textContent = document.getElementById('textToPost').value;
+
+    let jwt = localStorage.getItem('jwt');
+    console.log(jwt);
+
+
+    
+    try {
+    
+    const response = await axios({
+        method: 'POST',
+        url: "http://localhost:3000/public/posts/"+jwt+textContent,
+        data: {
+            data: {
+                "type": "post",
+                "body": textContent
+            }
+          
+        },
+      });
+    }
+    catch(error) {
+        alert(error);
+    }
+
+    //handleWelcomeButtonPress(event);
+
+      
+
+      /*
+
+     let r = axios.post('http://localhost:3000/public/', {
+        
+        data: {
+            fname: textContent,
+            lname: "renis",
+         
+        }
+    });
+    */
 
 }
 
 export async function handleWelcomeButtonPress(event) {
 
-  console.log("Welcome, " + decode.data.fname);
+  //console.log("Welcome, " + decode.data.fname);
 
     
     
@@ -243,8 +423,44 @@ export async function handlePostsTabClick(event) {
         </div>
         
     `;
+    
+        const response = await axios({
+            method: 'GET',
+            url: "http://localhost:3000/public/posts",
+          });
+        
 
-    for(let i=0; i<50; i++){
+    console.log(response.data.result);
+
+    for(var key in response.data.result) {
+        if(response.data.result.hasOwnProperty(key)) {
+                    r+=`
+            <div class="is-profile-card">
+            <div class="columns">
+                <div class="column">
+                        <img class="is-profile-pic" width="50" height="50" src="OldWell.png">
+                </div>
+                <div class="column">
+                    <b>Twig Man</b></br>
+                    <span style:"color: #F8DA17">Senior</span>
+                </div>
+                <div class="column">
+                    ${response.data.result[key].body}
+                </div>
+                <div class="column">
+                    <button type="button" class="button is-warning" id="request">Request</button>
+                </div>
+            </div>
+            </div>
+            `;
+                    console.log(response.data.result[key].body);
+            
+            
+        }
+    }
+
+/*
+    for(let i=0; i<100; i++){
         //alert("good lord");
         r+=`
             <div class="is-profile-card">
@@ -257,7 +473,7 @@ export async function handlePostsTabClick(event) {
                     <span style:"color: #F8DA17">Senior</span>
                 </div>
                 <div class="column">
-                    Lopper Dopper!
+                    ${response.data.result.body}
                 </div>
                 <div class="column">
                     <button type="button" class="button is-warning" id="request">Request</button>
@@ -266,6 +482,7 @@ export async function handlePostsTabClick(event) {
             </div>
             `;
     }
+    */
 
 }
 else {
@@ -397,6 +614,7 @@ else {
     r+=`</div>`;
 
     $('#appBox').replaceWith(r);
+    $(document).off();
     $root.on('click', "#users", handlePostsTabClick);
 
 
